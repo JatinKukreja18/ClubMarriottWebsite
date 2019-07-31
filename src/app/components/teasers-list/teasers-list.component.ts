4294d4ce4b5a9df7859387f57583679fb0eba376
jatin kukreja <jatinkukreja18@yahoo.in>
2019-08-01 04:33:12 +0530

    mobile responsive for banner carousel, teaser listing, mockup banner, experience carousel

--- src/app/components/teasers-list/teasers-list.component.ts
+++ src/app/components/teasers-list/teasers-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+declare var $:any;
+import "./teasers-list.js";
 @Component({
   selector: 'cm-teasers-list',
   templateUrl: './teasers-list.component.html',
