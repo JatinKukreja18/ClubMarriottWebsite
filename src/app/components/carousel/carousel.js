2037b0229555eeb8e59c55727b27be26b8592fd2
jatin kukreja <jatinkukreja18@yahoo.in>
2019-09-13 19:29:33 +0530

    added code in js for active class

--- src/app/components/carousel/carousel.js
+++ src/app/components/carousel/carousel.js
@@ -1,8 +1,17 @@
 
 $(document).ready(function(){
-  $('.carousel#cmBannerCarousel').carousel({
-    interval: 5000,
-    pause:true,
-    autoplay: false
-  })
+
+  console.log($('.cm-carousel .carousel')[0].id);
+  for(var i = 0 ; i < $('.cm-carousel .carousel').length; i++){
+    $('.cm-carousel .carousel')[i].id
+    console.log($('.cm-carousel .carousel')[i].id);
+    $('#'+ $('.cm-carousel .carousel')[i].id + ' .carousel-item')[0].classList.add('active')
+
+    if($('#'+ $('.cm-carousel .carousel')[i].id + ' .carousel-item').length == 1){
+      $('#'+ $('.cm-carousel .carousel')[i].id + ' .carousel-control-prev')[0].classList.add('is-hidden')
+      $('#'+ $('.cm-carousel .carousel')[i].id + ' .carousel-control-next')[0].classList.add('is-hidden')
+      $('#'+ $('.cm-carousel .carousel')[i].id + ' .carousel-indicators')[0].classList.add('is-hidden')
+    }
+
+  }
 })
