6a9a70950494d7bd1bbfd7fddcef9c64722c4f18
jatin kukreja <jatinkukreja18@yahoo.in>
2020-04-24 02:16:04 +0530

    benefits page

--- src/app/components/blog-thumbnail/blog-thumbnail.component.ts
+++ src/app/components/blog-thumbnail/blog-thumbnail.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'cm-blog-thumbnail',
   templateUrl: './blog-thumbnail.component.html',
   styleUrls: ['./blog-thumbnail.component.css']
 })
 export class BlogThumbnailComponent {
+  @Input() align;
 }
