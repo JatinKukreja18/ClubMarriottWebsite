246f067df17ce1ef4185ebfbc9d20f5fc327a83a
jatin kukreja <jatinkukreja18@yahoo.in>
2020-08-12 03:21:38 +0530

    triggered play with jqeury

--- src/app/components/carousel-type2/carousel.js
+++ src/app/components/carousel-type2/carousel.js
@@ -1,17 +1,28 @@
 
-$(document).ready(function(){
-//   $('.carousel.carousel-type-two').carousel({
-//     interval: 5000,
-//     // pause:false
-//   })
-$('.owl-carousel.owl-exp').owlCarousel({
-  margin:10,
-  loop:true,
-  autoWidth:true,
-  items:2,
-  dots:false,
-  center:true,
-  nav:true,
-  smartSpeed: 1000
-})
+$(document).ready(function () {
+  //   $('.carousel.carousel-type-two').carousel({
+  //     interval: 5000,
+  //     // pause:false
+  //   })
+  $('.owl-carousel.owl-exp').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    items: 2,
+    dots: false,
+    center: true,
+    nav: true,
+    smartSpeed: 1000
+  })
+  var hasScrolled = false;
+  window.addEventListener('scroll', function () {
+    if (!hasScrolled) {
+      var allvideos = document.querySelectorAll('.owl-item video');
+      for (i = 0; i < allvideos.length; i++) {
+        allvideos[i].play()
+        console.log('here');
+      }
+      hasScrolled = true;
+    }
+  })
 })
